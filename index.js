1e6467f889c6822a3ef3ef94a7e0857b0d5269cd
Adam Clarkson <atclarks@iu.edu>
2021-07-20 17:31:27 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const electron = require('electron')
+
+const { app, BrowserWindow, ipcMain } = electron // <-- made for free for us
+
+/* 
+ Thing we're listening too
+ |
+ |       Event we're listening for  
+ |       |      
+ |       |        Function to run when event occurs
+ |       |        |
+ V       V        V    */
+app.on('ready', () => {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true, // is default value after Electron v5
+      contextIsolation: false, // protect against prototype pollution
+      enableRemoteModule: true, // turn off remote },
+    },
+  })
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+})
